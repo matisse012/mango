b0e4f0a35fbb21848b7984919e61d28576fcfa62
matisse012 <89430171+matisse012@users.noreply.github.com>
2021-08-24 12:29:10 +0900

    Update index.js
    make Vue.js for login form ;

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
 let login = new Vue({
-el:"vue_login",
+el:"#vue_login",
 data:{
   mi:"",
   mp:""
 },
 methods:{
   login_frm:function(){
-  
+    if(!this.mi){
+     alert("아이디를 입력하세요"); 
+    }
+    else if(!this.mp){
+     alert("패스워드를 입력하세요"); 
+    }
+    else{
+     alert("ok"); 
+    }
   }
 },
 computed:{
